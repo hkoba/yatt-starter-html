229721735a1626da6e51eeea09eee67d29e269f0
Kobayasi, Hiroaki <buribullet@gmail.com>
2025-05-18 09:22:08 +0900

    Build only if the tagged image doesn't yet exist

--- demo.zsh
+++ demo.zsh
@@ -8,7 +8,9 @@ cd $0:h
 
 myTag=my-yatt-test1
 
-podman build -f Dockerfile -t $myTag
+if ! podman image exists $myTag; then
+    podman build -f Dockerfile -t $myTag
+fi
 
 podman run -it --rm -p 5000:5000 \
        -v $PWD/public:/myapp/public:O \
